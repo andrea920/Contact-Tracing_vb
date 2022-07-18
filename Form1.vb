240f29abb134ce378f03bdf57672b1cbf970fdfb
Ma. Andrea G. Perido <andreamaperido@gmail.com>
2022-07-19 03:39:30 +0800

    generating txt file

--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,14 @@
-﻿Public Class Form1
+﻿Imports System.IO
+Public Class Form1
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+        Dim Entry As StreamWriter = New StreamWriter(Application.StartupPath & "\PeopleEntry\" & "Data.txt")
 
+
+        Entry.WriteLine(lblName.Text & " " + tbxName.Text)
+        Entry.WriteLine(lblAddress.Text & " " + tbxAddress.Text)
+        Entry.WriteLine(lblCnum.Text & " " + tbxCnum.Text)
+        Entry.WriteLine(lblTemp.Text & " " + tbxTemp.Text)
+
+
+    End Sub
 End Class
